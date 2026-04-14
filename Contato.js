05b9c2f415dfe507d007b0f4a18dbee048d0fce5
Mahcgarcia <mayara.garcia89@hotmail.com>
2026-04-13 22:48:38 -0300

    Add files via upload

---
+++ Contato.js
@@ -0,0 +1,16 @@
+function Contato() {
+  return (
+    <section>
+      <h2>Contato</h2>
+
+      <form>
+        <input type="text" placeholder="Seu nome" />
+        <input type="email" placeholder="Seu email" />
+        <textarea placeholder="Mensagem"></textarea>
+        <button>Enviar</button>
+      </form>
+    </section>
+  );
+}
+
+export default Contato;
