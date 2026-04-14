946269c929635f40a98b886d6d6efe5e41eced10
Mahcgarcia <mayara.garcia89@hotmail.com>
2026-04-13 23:46:02 -0300

    Add files via upload

---
+++ src/components/Sobre.js
@@ -0,0 +1,15 @@
+function Sobre() {
+  return (
+    <section id="sobre">
+      <h2>Sobre Mim</h2>
+
+      <img src="minha-foto.jpg.jpeg" alt="Mayara Garcia" />
+
+      <p>
+        Sou estudante de Sistemas de Internet e estou iniciando minha carreira como desenvolvedora web.
+      </p>
+    </section>
+  );
+}
+
+export default Sobre;
